14610bcc6b7b26207a937dee4993a740cf74ff6d
shampeak <976107540@qq.com>
2016-08-10 18:12:04 +0800

    0.0007

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -3,9 +3,47 @@ include("../vendor/autoload.php");
 
 define('APPROOT', '../App/');
 
-//´íÎóÌáÊ¾
+//é”™è¯¯æç¤º
 $error_reporting       = E_ALL ^ E_NOTICE;
 ini_set('error_reporting', $error_reporting);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('Stand');
+$log->pushHandler(new StreamHandler('../Cache/Log/log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
+
+
+exit;
+
+
+
+
+
+
+
+
+
 
 App\Bootstrap::run();
 
